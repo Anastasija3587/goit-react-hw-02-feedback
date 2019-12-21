78a2a18d5850c6241ab2a3eacf64ccc1b02d3cd1
Anastasija3587 <Anastasija3587gmail.com>
2019-12-21 17:58:40 +0200

    Updates

---
+++ precache-manifest.c44287ad55a559c31ba2c4ea723b3044.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1a856ca18a13465437f5f31755094b9",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "c582e3425d6f69ffa09a",
+    "url": "/goit-react-hw-02-feedback/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "48d13c9f03b779feb390",
+    "url": "/goit-react-hw-02-feedback/static/css/main.7bbf32be.chunk.css"
+  },
+  {
+    "revision": "c582e3425d6f69ffa09a",
+    "url": "/goit-react-hw-02-feedback/static/js/2.3ed03f99.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-02-feedback/static/js/2.3ed03f99.chunk.js.LICENSE"
+  },
+  {
+    "revision": "48d13c9f03b779feb390",
+    "url": "/goit-react-hw-02-feedback/static/js/main.fabf16d0.chunk.js"
+  },
+  {
+    "revision": "4c3d7c3ac1ad4fad6b67",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.77b613c8.js"
+  }
+]);
